0ba0beb8053a95394f1836b84a7688048ddbd0c7
brian presta <brian.presta@gmail.comgit commit -m first>
2020-06-04 13:37:08 -0700

    set up character pools, function to ask for pw length

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
 // Assignment code here
-
-
+var characterPools = {
+  lower:"abcdefghijklmnopqrstuvwxyz",
+  upper:"ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  numbers:"0123456789",
+  special:" !#$%&'()*+,-./:;<=>?@[\]^_`{|}~" + '"'
+}
+var askLength = function(){
+  length = Number(window.prompt("How many characters should your password contain?"))
+  if (!length){
+    window.alert("Please enter a number")
+    length = askLength()
+  }
+  if (length < 8) {
+    window.alert("Passwords must be at least 8 characters long, please enter a new length.")
+    length = askLength()
+  }
+  if (length > 128) {
+    window.alert("Passwords may not be longer than 128 characters, please enter a new length.")
+    length = askLength()
+  }
+  // return password
+};
+var generatePassword = function() {
+  pwLength = Math.floor(askLength())
+  availableCharacters = characterPools.lower
+  return pwLength
+  // var upperYesNo  = window.prompt()
+};
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
